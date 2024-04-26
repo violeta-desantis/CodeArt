492047654d3d72723c29872bdcd304c5cc4baadb
Violeta (vi) DeSantis <61853817+violeta-desantis@users.noreply.github.com>
2024-04-25 23:28:47 -0400

    add sierpinski code golf art

---
+++ sierpinski/main.c
@@ -0,0 +1,12 @@
+                      x
+                    ,   y
+                  ,       i
+                ;main(){for (
+              y               =  
+            16  ;           y  --
+          ;    puts       (      ""
+        )){for(i=x=0;i  ++<y;)putchar
+      (               32              )
+    ; for           (   ;           x   +
+  y       <       8       +       8       ;
+)  printf ( x   ++ & y ?"  ":   "▲ " )  ; } }
